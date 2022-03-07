e48e7e78152e89857b5cbc64bab5e03a003599fc
Nico Blasutig <nicolasblasutig@gmail.com>
2022-03-07 15:46:21 -0300

    features

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
   return (
     <>
     <p><a href='#home'>Home</a></p>
-    <p><a href='#wgp3'>What is GPT?</a></p>
+    <p><a href='#wgpt3'>What is GPT?</a></p>
     <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#features'>Case Studies</a></p>
     <p><a href='#blog'>Library</a></p>
